433283b2c2f72a5d1121839d9db75162a343eea4
jordanv <jordanv@mmtoolandmold.com>
2015-11-04 12:58:10 -0600

    Added method call header

---
+++ Common/MethodCallHeader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MethodCallHeader
+    {
+        public uint Version;
+        //public MethodCallOptions Options;
+        public ulong InterfaceId;
+        public ulong MethodId;
+
+        public MethodCallHeader(uint version, ulong interfaceId, ulong methodId)
+        {
+            Version = version;
+            //Options = options;
+            InterfaceId = interfaceId;
+            MethodId = methodId;
+        }
+
+        public static unsafe int SizeOf => sizeof(MethodCallHeader);
+    }
+}
